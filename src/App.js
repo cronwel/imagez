5caa4940564284b5399f9c1a6f43166bb73e2849
cronwel <noelirias@gmail.com>
2019-08-15 15:55:45 -0500

    Updates to package.json It appears there where a few changes that needed to be made to out date packages.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Menu from './components/01-Menu/Menu';
 import { ReactComponent as Logo } from './resources/a_01_images/gymcal_logo.svg';
@@ -6,23 +6,20 @@ import Signup from './components/04-Frontpage/signup';
 import Credits from './components/04-Frontpage/credits';
 import Download from './components/04-Frontpage/download';
 import Tagline from './components/04-Frontpage/tagline';
-import axios from 'axios';
-class App extends Component {
 
-  componentDidMount() {
-    axios.get('/profile/')
-  }
+
+class App extends React.Component {
   render() {
-    return (
-      <div className="App">
-        <Logo className="logoStyle" />
-        <Tagline />
-        <Menu />
-        <Signup />
-        <Credits />
-        <Download />
-      </div>
-    );
+  return (
+    <div className="App">
+      <Logo className="logoStyle" />
+      <Tagline />
+      <Menu />
+      <Signup />
+      <Credits />
+      <Download />
+    </div>
+  );
   }
 }
 
