7d30a8fcab89901ff179c7559906b5871dba054e
cronwel <noelirias@gmail.com>
2019-10-08 21:50:35 -0500

    Git Error: Another git process seems to be running in this repository, e.g. an editor opened by 'git commit'. Please make sure all processes are terminated then try again. If it still fails, a git process may have crashed in this repository earlier: remove the file manually to continue. ========== I've removed this file per StackOverflow https://stackoverflow.com/questions/38004148/another-git-process-seems-to-be-running-in-this-repository

--- src/components/01-Menu/Menu.js
+++ src/components/01-Menu/Menu.js
@@ -1,19 +1,18 @@
 import React, {Component} from 'react';
 import "./menu.css";
 
-
 class Menu extends Component {
-render() {
-  return (
-    <nav className="menu">
-        <a href="http://" target="_blank" rel="noopener noreferrer">What</a>
-        <a href="http://" target="_blank" rel="noopener noreferrer">Details</a>
-        <a href="http://" target="_blank" rel="noopener noreferrer">Talk</a>
-    </nav>
-  )
-}
-
-
+  render(){
+    return (
+      <nav >
+        <div className="menu">
+          <a href="http://" target="_blank" rel="noopener noreferrer">About</a>
+          <a href="http://" target="_blank" rel="noopener noreferrer">Details</a>
+          <a href="http://" target="_blank" rel="noopener noreferrer">Contact</a>
+        </div>
+      </nav>
+    )
+  }
 }
 
 export default Menu;
--- src/components/03-Workouts/Workout.js
+++ src/components/03-Workouts/Workout.js
@@ -4,11 +4,15 @@ class Workout extends Component {
   render() {
     return(
       <section className="section">
+        <div>
+          <p>
         This will be the section header
+          </p>
+        </div>
       </section>
     )
   }
-
 }
+
 
 export default Workout;
